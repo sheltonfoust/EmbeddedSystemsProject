7a377cd565d8b19633653f47e19817a6dca5b589
sheltonfoust <sheltonfoust@gmail.com>
2023-09-03 17:08:52 -0500

    Initial commit

---
+++ Debug/uartecho.d
@@ -0,0 +1,51 @@
+# FIXED
+
+uartecho.obj: ../uartecho.c
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+uartecho.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/GPIO.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/UART.h
+uartecho.obj: syscfg/ti_drivers_config.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/devices/DeviceFamily.h
+
+../uartecho.c:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/UART.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/devices/DeviceFamily.h:
+
---
+++ uartecho.c
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2015-2020, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ======== uartecho.c ========
+ */
+#include <stdint.h>
+#include <stddef.h>
+
+/* Driver Header files */
+#include <ti/drivers/GPIO.h>
+#include <ti/drivers/UART.h>
+
+/* Driver configuration */
+#include "ti_drivers_config.h"
+
+/*
+ *  ======== mainThread ========
+ */
+
+#define VERSION "1.0"
+#define ASSIGNMENT 1
+#define AUTHOR "Shelton Foust"
+#define MSG_LEN 50
+
+
+
+void *mainThread(void *arg0)
+{
+
+    UART_Handle uart;
+    UART_Params uartParams;
+
+    /* Call driver init functions */
+    GPIO_init();
+    UART_init();
+
+    /* Configure the LED pin */
+    GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
+
+    /* Turn on user LED */
+    GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
+
+    /* Create a UART with data processing off. */
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.baudRate = 115200;
+
+    uart = UART_open(CONFIG_UART_0, &uartParams);
+
+    if (uart == NULL)
+    {
+        /* UART_open() failed */
+        while (1);
+    }
+
+
+    char        inputChar;
+    char        inputLine[MSG_LEN + 1];
+    inputLine[0] = 0;
+    char        aboutStr[] = "-about";
+    char        helpStr[] = "-help";
+    int         lineLength = 0;
+    while (1)
+    {
+        UART_read(uart, &inputChar, 1);
+        UART_write(uart, &inputChar, 1);
+
+        if (lineLength > MSG_LEN)
+        {
+            char output[] = "\n\rOVERFLOW\n\r";
+            UART_write(uart, &output, strlen(output));
+            lineLength = 0;
+            inputLine[lineLength] = 0;
+        }
+        else if (inputChar == '\n' || inputChar == '\r')
+        {
+            char output[MSG_LEN];
+            sprintf(output, "\n\r");
+            UART_write(uart, &output, strlen(output));
+
+            if (stringStartsWith(inputLine, aboutStr))
+            {
+
+                sprintf(output, "Author: %s\n\r", AUTHOR);
+                    UART_write(uart, &output, strlen(output));
+                    sprintf(output, "Version: %s\n\r", VERSION);
+                    UART_write(uart, &output, strlen(output));
+                    sprintf(output, "Assignment: %d\n\r", ASSIGNMENT);
+                    UART_write(uart, &output, strlen(output));
+                    sprintf(output, "Compile Timestamp: %s %s\n\r\n\r", __TIME__, __DATE__);
+                    UART_write(uart, &output, strlen(output));
+
+            }
+            else if (stringStartsWith(inputLine, helpStr))
+            {
+                sprintf(output, "\t%s\n\r", aboutStr);
+                UART_write(uart, &output, strlen(output));
+                sprintf(output, "\t%s\n\r\n\r", helpStr);
+                UART_write(uart, &output, strlen(output));
+
+            }
+            else
+            {
+                char output[MSG_LEN];
+                sprintf(output, "Operation \"%s\" not valid.\n\r\n\r", inputLine);
+                UART_write(uart, &output, strlen(output));
+            }
+            lineLength = 0;
+            inputLine[lineLength] = 0;
+
+        }
+        else if (inputChar == '\b' || inputChar == 127)
+        {
+            if(lineLength > 0)
+            {
+                inputLine[lineLength] = 0;
+                lineLength--;
+            }
+        }
+        else
+        {
+            inputLine[lineLength] = inputChar;
+            inputLine[lineLength + 1] = 0;
+            lineLength++;
+        }
+
+    }
+}
+
+int stringStartsWith(char *str1, char *str2)
+{
+    if(strlen(str1) == strlen(str2))
+    {
+        char substring[strlen(str2) + 1];
+        strncpy(substring, str1, strlen(str2));
+        substring[strlen(str2)] = 0;
+       return (!strcmp(substring, str2));
+    }
+    return 0;
+}
+
+
