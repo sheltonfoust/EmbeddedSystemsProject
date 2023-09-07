d9afa8133af8f43ec010ce9924e5afea427096e7
sheltonfoust <sheltonfoust@gmail.com>
2023-09-07 09:11:02 -0500

    Fixed Issues Created by Making Git
    Deleted all files (except .git) and pasted them from homework submission.

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,27 +8,27 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs" --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs/Debug" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/third_party/CMSIS/Include" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos/posix" --include_path="C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --advice:power=none -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs/Debug/syscfg" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject" --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject/Debug" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/third_party/CMSIS/Include" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos/posix" --include_path="C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --advice:power=none -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject/Debug/syscfg" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
-build-1928100815: ../uartecho.syscfg
+build-1229091999: ../uartecho.syscfg
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: SysConfig'
-	"C:/ti/ccs1240/ccs/utils/sysconfig_1.17.0/sysconfig_cli.bat" -s "C:/ti/simplelink_msp432e4_sdk_4_20_00_12/.metadata/product.json" --script "C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs/uartecho.syscfg" -o "syscfg" --compiler ccs
+	"C:/ti/ccs1240/ccs/utils/sysconfig_1.17.0/sysconfig_cli.bat" -s "C:/ti/simplelink_msp432e4_sdk_4_20_00_12/.metadata/product.json" --script "C:/Users/sfous/workspace_v12/EmbeddedSystemsProject/uartecho.syscfg" -o "syscfg" --compiler ccs
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
-syscfg/ti_drivers_config.c: build-1928100815 ../uartecho.syscfg
-syscfg/ti_drivers_config.h: build-1928100815
-syscfg/ti_utils_build_linker.cmd.exp: build-1928100815
-syscfg/syscfg_c.rov.xs: build-1928100815
-syscfg/: build-1928100815
+syscfg/ti_drivers_config.c: build-1229091999 ../uartecho.syscfg
+syscfg/ti_drivers_config.h: build-1229091999
+syscfg/ti_utils_build_linker.cmd.exp: build-1229091999
+syscfg/syscfg_c.rov.xs: build-1229091999
+syscfg/: build-1229091999
 
 syscfg/%.obj: ./syscfg/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs" --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs/Debug" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/third_party/CMSIS/Include" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos/posix" --include_path="C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --advice:power=none -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="syscfg/$(basename $(<F)).d_raw" --include_path="C:/Users/sfous/workspace_v12/uartecho_MSP_EXP432E401Y_nortos_ccs/Debug/syscfg" --obj_directory="syscfg" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject" --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject/Debug" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/third_party/CMSIS/Include" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos" --include_path="C:/ti/simplelink_msp432e4_sdk_4_20_00_12/kernel/nortos/posix" --include_path="C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include" --advice:power=none -g --diag_warning=225 --diag_warning=255 --diag_wrap=off --display_error_number --gen_func_subsections=on --preproc_with_compile --preproc_dependency="syscfg/$(basename $(<F)).d_raw" --include_path="C:/Users/sfous/workspace_v12/EmbeddedSystemsProject/Debug/syscfg" --obj_directory="syscfg" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
